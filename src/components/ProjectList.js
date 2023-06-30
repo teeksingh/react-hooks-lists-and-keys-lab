286a561d61d3a273f933066aa3ea58e4002ad838
Prateek <teeksingh@gmail.com>
2023-06-30 18:28:03 -0500

    projectList complete

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,18 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  //console.log(projects);
+
+  const projectsComp = projects.map((project) => {
+    return <ProjectItem key={project.id} id={project.id} name={project.name} about={project.about} technologies={project.technologies} />
+  })
+
+  // console.log(projectsComp)
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectsComp}</div>
     </div>
   );
 }
